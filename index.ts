cdcc9286e9efa18f42a5ffa53a5dba428449d684
Keller18306 <kompeter17@gmail.com>
2021-08-07 22:05:49 +0300

    new line fix

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ function connection(socket: Socket) {
 
     const time = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')},${date.getMilliseconds().toString().padStart(3, '0')}`
 
-    appendFileSync('logs.txt', `[${time}]: ${ip} on port ${port}`)
+    appendFileSync('logs.txt', `[${time}]: ${ip} on port ${port}\n`)
 
     socket.write('Idi nahui')
 
