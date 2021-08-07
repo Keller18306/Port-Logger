33d5f889baee39de6dfc1e61c8bab7cb586486f3
Keller18306 <kompeter17@gmail.com>
2021-08-07 21:56:01 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,45 @@
+import { appendFileSync } from 'fs'
+import { createServer, Socket } from 'net'
+
+const ports: number[] = [
+    20,
+    21,
+    22,
+    23,
+    1111,
+    2222,
+    3306,
+    2222,
+    3333,
+    4444,
+    5555,
+    6666,
+    7777,
+    8888,
+    9999,
+    19132,
+    25565,
+    27010,
+    27015
+]
+
+function connection(socket: Socket) {
+    const date = new Date()
+
+    const ipParts = socket.remoteAddress!.split(':')
+    const ip = ipParts[ipParts.length-1]
+
+    const port = socket.localPort
+
+    const time = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')},${date.getMilliseconds().toString().padStart(3, '0')}`
+
+    appendFileSync('logs.txt', `[${time}]: ${ip} on port ${port}`)
+
+    socket.write('Idi nahui')
+
+    socket.destroy()
+}
+
+for(const port of ports) createServer(connection).listen(port, () => {
+    console.log(`Start server on ${port}`)
+})
